397f53f3f337cb1395f3f512a555eb93ec0257c3
Lianfen Shi <lianfen.shi@f1006-13-34173.bmcc.cuny.edu>
2015-12-01 21:12:33 -0500

    first commit

---
+++ page-about-me.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying all about me page.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package final_theme
+ */
+
+get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+
+		<div class="aboutme-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'final-theme' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+
+		<!--<footer class="entry-footer">
+			<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', 'final-theme' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+	</article><!-- #post-## -->
+
+	<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
